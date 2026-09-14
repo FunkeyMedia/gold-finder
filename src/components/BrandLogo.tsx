04bb4484a19a104d144819ecddd62188ad879cd5
Pascal Weyers <pascalweyers@MacBook-Air-von-Pascal.local>
2026-09-14 16:23:12 +0200

    Replace logo with minimal line gold bar

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function BrandLogo(){
- return <span className="brand-lockup"><Image src="/brand/gold-finder-logo.png" alt="" width={42} height={42} priority/><span>GOLD FINDER</span></span>
+ return <span className="brand-lockup"><Image src="/brand/gold-finder-line-bar.svg" alt="" width={42} height={42} priority/><span>GOLD FINDER</span></span>
 }
